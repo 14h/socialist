4b6257713c16af35258a8fe77b41dc13fbe4a3f5
Abdallah Kawji <abdallah@styla.com>
2020-05-16 19:56:02 +0200

    darkMode

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { addWebpackAlias, override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { getThemeVariables } = require('antd/dist/theme');
 
 // Add just the necessary icons to decrease bundle size
 function overrides(config, env) {
@@ -26,20 +27,9 @@ module.exports = override(
   addLessLoader({
     paths: ['./src/styles', './node_modules'],
     javascriptEnabled: true,
-    modifyVars: {
-      '@primary-color': '#3f51b5',
-      '@link-color': '#3f51b5',
-      '@success-color': '#52c41a',
-      '@warning-color': '#faad14',
-      '@error-color': '#f5222d',
-      '@font-size-base': '14px',
-      '@heading-color': 'rgba(0, 0, 0, .85)',
-      '@text-color': 'rgba(0, 0, 0, .65)',
-      '@text-color-secondary ': 'rgba(0, 0, 0, .45)',
-      '@disabled-color': 'rgba(0, 0, 0, .25)',
-      '@border-radius-base': '4px',
-      '@border-color-base': '#d9d9d9',
-      '@box-shadow-base': '0 2px 8px rgba(0, 0, 0, .15)',
-    },
+    modifyVars: getThemeVariables({
+        dark: true,
+        compact: false,
+    }),
   }),
 );
--- web/src/screens/Surveys/index.tsx
+++ web/src/screens/Surveys/index.tsx
@@ -1,13 +1,55 @@
 import React, { useState } from 'react';
-import { Breadcrumb, Button, Layout, Modal, Popconfirm, Space, Table } from 'antd';
+import { Breadcrumb, Button, Form, Input, Layout, message, Modal, Popconfirm, Space, Table } from 'antd';
 
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
 const { Content } = Layout;
 
-const deleteSurvey = (id: string) => {
-    console.log(`Survey with id ${id} has been deleted`)
+const deleteSurvey = async (id: string) => {
+    const hide = message.loading(`Deleting ${id}..`, 0);
+
+    try {
+        await new Promise((resolve, _reject) => {
+            setTimeout(() => resolve('survey_1'), 3000);
+        });
+        hide();
+        message.success(`${id} got successfully deleted!`);
+    } catch(error) {
+        hide();
+        message.error(`Failed to delete ${id}: ${error.message}`)
+    }
+}
+
+const createSurvey = async (title: string, history: any) => {
+    const hide = message.loading('Creating survey..', 0);
+    try {
+        const newSurveyId = await new Promise((resolve, _reject) => {
+            setTimeout(() => resolve('survey_1'), 3000);
+        });
+        hide();
+        message.success(`${title} got successfully created!`);
+        history.push(`/surveys/${newSurveyId}`);
+    } catch(error) {
+        hide();
+        message.error(`Failed to create ${title}: ${error.message}`)
+    }
+}
+
+const duplicateSurvey = async (id: string) => {
+    const hide = message.loading(`Duplicating survey ${id} ..`, 0);
+    try {
+        const newSurveyId = await new Promise((resolve, _reject) => {
+            setTimeout(() => resolve('survey_1'), 3000);
+        });
+        hide();
+        message.success(`${newSurveyId} got successfully duplicated!`);
+        // history.push(`/surveys/${newSurveyId}`);
+    } catch(error) {
+        hide();
+        message.error(`Failed to duplicated: ${error.message}`)
+    }
 }
 
 const SURVEYS_LIST = [
@@ -16,35 +58,40 @@ const SURVEYS_LIST = [
         key: 'survey_1',
         title: 'Global Drug Survey 2020',
         responses: 1,
-        lastEdited: 1581635524722,
+        updatedAt: 1581635524722,
+        createdAt: 1581615521722,
     },
     {
         id: 'survey_2',
         key: 'survey_2',
         title: 'Global Drug Survey 2021 new Platform test',
         responses: 100100,
-        lastEdited: 1289625564722,
+        updatedAt: 1289625564722,
+        createdAt: 1289615561722,
     },
     {
         id: 'survey_3',
         key: 'survey_3',
         title: 'Global Drug Survey: The Big UK Cannabis Survey 2019',
         responses: 222,
-        lastEdited: 1589631564222,
+        updatedAt: 1589631564222,
+        createdAt: 1589611561222,
     },
     {
         id: 'survey_4',
         key: 'survey_4',
         title: 'Global Drug Survey 2020 - copy',
         responses: 1235,
-        lastEdited: 1588635562722,
+        updatedAt: 1588635562722,
+        createdAt: 1588615561722,
     },
     {
         id: 'survey_4',
         key: 'survey_4',
         title: 'Global Drug Survey 2020 - copy - Global Drug Survey 2020 - copy Global Drug Survey 2020 - copy Global Drug Survey 2020 - copy',
         responses: 1235,
-        lastEdited: 1588635562722,
+        updatedAt: 1588635562722,
+        createdAt: 1588615561722,
     },
 ];
 
@@ -61,26 +108,35 @@ const columns = [
     },
     {
         title: 'Last Edited',
-        dataIndex: 'lastEdited',
-        render: (lastUpdated: number) => {
-            console.log(lastUpdated)
-            const date = new Date(lastUpdated);
-            console.log(date.getDate())
-            return (
-                <span>{date.toDateString()}</span>
-            )
-        },
-        sorter: (a: any, b: any) => a.lastEdited - b.lastEdited,
+        dataIndex: 'updatedAt',
+        render: (lastUpdated: number) => (
+            <span>{(new Date(lastUpdated)).toDateString()}</span>
+        ),
+        sorter: (a: any, b: any) => a.updatedAt - b.updatedAt,
+    },
+    {
+        title: 'Date created',
+        dataIndex: 'createdAt',
+        render: (lastUpdated: number) => (
+            <span>{(new Date(lastUpdated)).toDateString()}</span>
+        ),
+        sorter: (a: any, b: any) => a.createdAt - b.createdAt,
     },
     {
         title: null,
         key: 'action',
-        render: (_text: any, _record: any) => (
+        render: (_text: any, record: any) => (
             <Space size="middle">
-                <Link to={`/surveys/${_record.id}`}>Edit</Link>
                 <Popconfirm
-                    title={`Delete survey ${_record.title}`}
-                    onConfirm={() => deleteSurvey(_record.id)}
+                    title={`Duplicate survey ${record.title}`}
+                    onConfirm={() => duplicateSurvey(record.id)}
+                >
+                    <a>Duplicate</a>
+                </Popconfirm>
+                <Link to={`/surveys/${record.id}`}>Edit</Link>
+                <Popconfirm
+                    title={`Delete survey ${record.title}`}
+                    onConfirm={() => deleteSurvey(record.id)}
                 >
                     <a>Delete</a>
                 </Popconfirm>
@@ -88,16 +144,13 @@ const columns = [
         ),
     },
 ];
+
 const Surveys = () => {
     const [showAddModal, setShowAddModal] = useState(false);
+    const history = useHistory();
+
     return (
         <Layout className="surveys-container-layout">
-            <Breadcrumb className="breadcrumb">
-                <Breadcrumb.Item>
-                    <Link to="/">Home</Link>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>Surveys</Breadcrumb.Item>
-            </Breadcrumb>
             <Content className="surveys-container-content">
                 <Button onClick={() => setShowAddModal(true)} type="primary" className="create-survey-button">
                     Create a survey
@@ -110,30 +163,34 @@ const Surveys = () => {
                 <Modal
                     title="Create survey"
                     visible={showAddModal}
-                    onOk={console.log}
                     onCancel={() => setShowAddModal(false)}
+                    footer={null}
                 >
                     <h2>What would you like to name this survey?</h2>
+                    <Form
+                        name="basic"
+                        initialValues={{ remember: true }}
+                        onFinish={(values: any) => createSurvey(values.title, history)}
+                        onFinishFailed={console.log}
+                    >
+                        <Form.Item
+                            name="title"
+                            rules={[{
+                                required: true,
+                                whitespace: true,
+                                message: 'Please input your a valid name for your survey!',
+                            }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item style={{textAlign: 'center', marginTop: '60px'}} >
+                            <Button type="primary" htmlType="submit" style={{width: '40%'}}>
+                                Create
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </Modal>
-                {/*<List*/}
-                {/*    itemLayout="horizontal"*/}
-                {/*    dataSource={SURVEYS_LIST}*/}
-                {/*    renderItem={item => (*/}
-                {/*        <List.Item*/}
-                {/*            actions={[*/}
-                {/*                <Link to={`/surveys/${item.id}`} key="survey-edit">*/}
-                {/*                    Edit*/}
-                {/*                </Link>,*/}
-                {/*                <a key="survey-item-delete">Delete</a>,*/}
-                {/*            ]}*/}
-                {/*        >*/}
-                {/*            <List.Item.Meta*/}
-                {/*                title={<span>{item.title}</span>}*/}
-                {/*                description={item.id}*/}
-                {/*            />*/}
-                {/*        </List.Item>*/}
-                {/*    )}*/}
-                {/*/>*/}
             </Content>
         </Layout>
     );
